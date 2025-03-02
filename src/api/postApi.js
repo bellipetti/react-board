5e47ec9db66e2e9f5eb5d9c00918f5f20ee8a011
kimsinok <kso@mz.co.kr>
2025-03-02 21:49:41 +0900

    modify

--- src/api/postApi.js
+++ src/api/postApi.js
@@ -1,7 +1,7 @@
 
 import axios from "axios";
 
-const API_SERVER_HOST = 'http://post-service:8080';
+const API_SERVER_HOST = 'http://post-service.postapp.svc.cluster.local:8080';
 // const API_SERVER_HOST = 'http://localhost:8080';
 
 const prefix = `${API_SERVER_HOST}/api/v1`;
