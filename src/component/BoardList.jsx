8a9e267fb735efc3ffe1c9c8da8eb446b9f6be8b
bellipetti <nkjok7@gmail.com>
2025-04-24 18:49:15 +0900

    update 1000+9

--- src/component/BoardList.jsx
+++ src/component/BoardList.jsx
@@ -106,7 +106,7 @@ const BoardList = () => {
                 <div className="main-visual">
                     <div>
                         <h1>AI 시대를 넘어, 미래를 향해</h1>
-                        <p>2025 중앙대학교 스타일 게시판</p>
+                        <p>2025 대학교 스타일 게시판</p>
                     </div>
                 </div>
 
